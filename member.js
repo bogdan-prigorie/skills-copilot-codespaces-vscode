3dbb38676c40829bbe20985eb5978fc1e29b506b
Bogdan P <55491558+prigix89@users.noreply.github.com>
2024-05-16 12:23:32 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var m = document.getElementById("member");
+    var s = document.getElementById("skills");
+    var p = document.getElementById("project");
+    var c = document.getElementById("contact");
+
+    s.style.display = "block";
+    m.style.display = "none";
+    p.style.display = "none";
+    c.style.display = "none";
+}
